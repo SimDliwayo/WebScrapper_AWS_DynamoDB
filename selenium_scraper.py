c7bcce0dac306846ad8da3e49d36c3f35869e9ea
Simphiwe Dliwayo <dliwayosim@gmail.com>
2025-05-18 22:40:23 +0200

    added simple configurations for msedge driver location, and dynamica config for the website

--- selenium_scraper.py
+++ selenium_scraper.py
@@ -4,19 +4,28 @@ from selenium.webdriver.edge.service import Service
 from datetime import datetime
 import time
 import boto3
+import json
+import os
+
+
+#Load some configs 
+with open('.config/config.json', 'r') as f:
+    config = json.load(f)
+
+driver_path_file = config['driver_path_file']
+with open(driver_path_file, 'r') as f:
+    edge_driver_path = f.read().strip()
+
 
 # Setup Edge Driver
-edge_driver_path = "C:PATH//Please//Yes//edgeDRIVER.exe//PATH"
 service = Service(executable_path=edge_driver_path)
 options = webdriver.EdgeOptions()
 driver = webdriver.Edge(service=service, options=options)
 
 # Target URL
-url = 'https://www.avo.africa/ecommerce/search/result-list?vertical=ESHOP&categoryId=WATER'
-driver.get(url)
+driver.get(config['target_url'])
 
-
-time.sleep(5)
+time.sleep(config['sleep_time'])
 
 # Get all product cards
 product_cards = driver.find_elements(By.CSS_SELECTOR, 'a[ui-card-product]')
@@ -28,7 +37,7 @@ table = dynamodb.Table("Avo_Water_Solution")
 
 count = 0
 for card in product_cards:
-    if count >= 10:
+    if count >= 3:
         break
 
     try:
